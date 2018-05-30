f1a0390e01588022fc7d7aaf7f9440e778b39056
Inna Shuster <inna_shuster@innas-mbp.lan>
2018-05-29 20:12:03 -0700

    Clear DB

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
-DATABASE_URI = 'postgres://dtkspgkwnstitr:30768626be4f87328de0f3166bec93321eff4649ab064f736a2fefb4974eccde@ec2-54-83-59-120.compute-1.amazonaws.com:5432/dfdq16ggrl0rb6'
+SQLALCHEMY_DATABASE_URI = 'mysql://bfc91fba5e05fd:daab9b5c@us-cdbr-iron-east-04.cleardb.net/heroku_5c45cf31225cea4?reconnect=true'
 # SQLALCHEMY_DATABASE_URI = 'mysql://root:Varenka2000@localhost/tic_tac_to'
 # HEROKU_POSTGRESQL_PURPLE_URL
